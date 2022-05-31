5e7656ff0883ae8a68bf1fff20d81dbd659b2fee
Dhivakar.Palanisamy <Dhivakar.Palanisamy@BRILAP4303.BRILLIO.COM>
2022-05-31 08:35:59 +0530

    Mavan

---
+++ src/test/java/com/brillio/Test/LoginTest.java
@@ -0,0 +1,17 @@
+package com.brillio.Test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+		
+    @Test
+	
+	public void validCredentialTest()
+	{
+		
+		System.out.println("Valid");
+		
+	}
+	
+
+}
